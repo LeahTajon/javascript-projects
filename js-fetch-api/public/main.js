6d8f3766a056bf693c86babc05e159a544c159fa
Leah Tajon <leahtajon@gmail.com>
2022-12-17 13:18:46 +1000

    Update files

--- js-fetch-api/public/main.js
+++ js-fetch-api/public/main.js
@@ -8,27 +8,30 @@ const responseField = document.querySelector('#responseField');
 
 // Asynchronous function
 const getSuggestions = () => {
-    const wordQuery = inputField.value;
-    const endpoint = url + wordQuery;
-
-    fetch(endpoint, {cahe: 'no-cache'})
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('Request failed!');
-        }, (networkError) => {
-            console.log(networkError.message);
-        });
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${wordQuery}`;
+  
+  fetch(endpoint, {cache: 'no-cache'}).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  })
+  .then((jsonResponse) => {
+    //renderRawResponse(jsonResponse);
+    renderResponse(jsonResponse);
+  })
 }
 
 // Clears previous results and display results to webpage
 const displaySuggestions = (event) => {
-    event.preventDefault();
-    while(responseField.firstChild) {
-        responseField.removeChild(responseField.firstChild);
-    }
-    getSuggestions();
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
 };
 
 submit.addEventListener('click', displaySuggestions);
