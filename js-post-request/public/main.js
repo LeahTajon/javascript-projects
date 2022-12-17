0df2e4f38b62a15f345c3e577d67eb76c2be1bb6
Leah Tajon <leahtajon@gmail.com>
2022-12-18 00:15:50 +1000

    Update files

--- js-post-request/public/main.js
+++ js-post-request/public/main.js
@@ -1,5 +1,5 @@
 // Information to reach API
-const apiKey = '<API Key>';
+const apiKey = 'b9b42545f6cf4349b9fd3c9c1a317f2c';
 const url = 'https://api.rebrandly.com/v1/links';
 
 // Some page elements
@@ -9,24 +9,35 @@ const responseField = document.querySelector('#responseField');
 
 // Asynchronous functions
 const shortenUrl = () => {
-    const urlToShorten = inputField.value;
-    const data = JSON.stringify({destination: urlToShorten});
-
-    fetch(url, {
-        method: 'POST',
-        headers: {'Content-type': 'application/json',
-    'apikey': apiKey},
-        body: data
-    });
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error ('Request failed!');
+  }, (networkError) => {
+    console.log(networkError.message);
+  }).then((jsonResponse) => {
+    renderResponse(jsonResponse);
+  });
 }
 
 // Clear page and call Asynchronous functions
 const displayShortUrl = (event) => {
-    event.preventDefault();
-    while(responseField.firstChild) {
-        responseField.removeChild(responseField.firstChild);
-    }
-    shortenUrl();
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
 }
 
-shortenButton.addEventListener('click', displayShortUrl)
+shortenButton.addEventListener('click', displayShortUrl);
